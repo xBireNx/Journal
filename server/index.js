80ad76c748aa0231bda5d901ac334c7892f145ff
Murgesh Ekunde <mekunde@gmail.com>
2024-02-19 21:40:10 +0530

    Db connection done

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// MongoDB connection URL
+const mongoURI = 'mongodb+srv://Biren:123%40Biren@cluster0.ckxmi.mongodb.net';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.get('/', (req, res) => {
     res.send('Successful response.');
@@ -8,3 +17,6 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Example app is listening on port 3000.');
 });
+
+
+module.exports = mongoose;
