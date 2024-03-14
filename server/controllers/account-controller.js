98b5435b6c3ed15eb8237e93188e146bba881ef0
PsyFucx <shashwat.biren007@gmail.com>
2024-03-14 23:34:16 +0530

    account controller

--- server/controllers/account-controller.js
+++ server/controllers/account-controller.js
@@ -6,26 +6,46 @@ exports.dashboard = async (req, res, next) => {
   try {
     const { accountNumber, broker, initialBalance, phase } = req.body;
 
+    // Assuming req.user contains the logged-in user's information
+    const userId = req.user._id;
+
     const existingAccount = await Account.findOne({ accountNumber });
 
     if (existingAccount) {
       return res.status(400).json({ message: "Account already exists" });
     }
+    // Function to calculate maxLoss based on broker
+    const calculateMaxLoss = (broker) => {
+      if (broker === "FundedNext") {
+        // Perform your calculation here
+        return 8; // Example calculation result
+      }
+      // Handle other cases if needed
+    };
 
-    let maxLossPer
-    const maxLoss = () => {
-        if(broker == "FundedNext"){
-            maxLossPer = 8 ;
-            return maxLossPer;
-        }
-    }
+    // Calculate maxLoss based on broker
+    const maxLoss = calculateMaxLoss(broker);
+
+    // Function to calculate maxLoss based on broker
+    const calculateDailyMaxLoss = (broker) => {
+      if (broker === "FundedNext") {
+        // Perform your calculation here
+        return 5; // Example calculation result
+      }
+      // Handle other cases if needed
+    };
+
+    // Calculate maxLoss based on broker
+    const dailyLoss = calculateDailyMaxLoss(broker);
 
     const newAccount = new Account({
       accountNumber,
       broker,
       initialBalance,
       phase,
-      maxLoss: maxLossPer,
+      maxLoss,
+      dailyLoss,
+      userId: userId, // Associate the account with the logged-in user
     });
 
     await newAccount.save();
@@ -36,4 +56,3 @@ exports.dashboard = async (req, res, next) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
